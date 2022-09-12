e10292e725acc400b19d9bf7cadf809e79c81eb3
hesham-salem <hesham1salem@gmail.com>
2022-09-12 16:50:00 +0200

    first commit

---
+++ app/app.cpp
@@ -0,0 +1,6 @@
+#include "app.h"
+
+int main(void)
+{
+    print("the result is :{} \n", add(5, 5));
+}
---
+++ include/app.h
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <fmt/core.h>
+#include "add.h"
+
+using namespace std;
+using namespace fmt;
